601ec44dcdeef7f930c088665263aa60630d0cb1
Zoltan Vigh <vigh.zoltan@codefactory.hu>
2025-02-05 21:24:27 +0100

    fix: add rails_icons

---
+++ config/initializers/rails_icons.rb
@@ -0,0 +1,36 @@
+RailsIcons.configure do |config|
+  config.default_library = "heroicons"
+  # config.default_variant = "" # Set a default variant for all libraries
+
+  # Override Heroicon defaults
+  # config.libraries.heroicons.outline.default.css = "size-6"
+  # config.libraries.heroicons.outline.default.stroke_width = "1.5"
+  # config.libraries.heroicons.outline.default.data = {}
+
+  # config.libraries.heroicons.solid.default.css = "size-6"
+  # config.libraries.heroicons.solid.default.data = {}
+
+  # config.libraries.heroicons.mini.default.css = "size-5"
+  # config.libraries.heroicons.mini.default.data = {}
+
+  # config.libraries.heroicons.micro.default.css = "size-4"
+  # config.libraries.heroicons.micro.default.data = {}
+
+  # Override Lucide defaults
+  # config.libraries.lucide.outline.default.css = "size-6"
+  # config.libraries.lucide.outline.default.stroke_width = "1.5"
+  # config.libraries.lucide.outline.default.data = {}
+
+  # Override Tabler defaults
+  # config.libraries.tabler.solid.default.css = "size-6"
+  # config.libraries.tabler.solid.default.data = {}
+
+  # config.libraries.tabler.outline.default.css = "size-6"
+  # config.libraries.tabler.outline.default.stroke_width = "2"
+  # config.libraries.tabler.outline.default.data = {}
+
+  # Override Radix defaults
+  # config.libraries.radix.default.css = "size-6"
+  # config.libraries.radix.default.stroke_width = "2"
+  # config.libraries.radix.default.data = {}
+end
